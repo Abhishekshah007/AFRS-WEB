dd194ae06bbc2288f5c1909b7b39a31fac475df9
Abhishek Shah <amitkumarshah499@gmail.com>
2026-06-19 23:23:01 +0530

    fixing image pattern issue

--- next.config.ts
+++ next.config.ts
@@ -34,10 +34,6 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: ''
-      }
     ],
   },
   webpack: (webpackConfig) => {
